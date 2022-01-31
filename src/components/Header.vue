07f9627dbf63bd5a9a84afe79e72c6b58dbe80dc
Sara-Barbi <barbisara02@gmail.com>
2022-01-31 16:50:51 +0100

    import

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="hello">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <img src="../assets/img/spotify-logo.png" alt="">
+        </div>
+       </div>
+    </div>
   </div>
 </template>
 
@@ -14,18 +21,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+@import '../assets/style/variables.scss';
+
+
+
+
+
+
+
+
+
+
+
+
+
 </style>
